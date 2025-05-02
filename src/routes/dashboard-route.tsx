b9cbc883404aeb09413567480c1b6393d1ffa060
GILANG <gaditia744@gmail.com>
2025-05-02 12:42:36 +0700

    ubah route

--- src/routes/dashboard-route.tsx
+++ src/routes/dashboard-route.tsx
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Navigate } from "react-router-dom";
 import { DashboardLayout } from "../layout/DashboardLayout";
 import ProductList from "../features/halaman-produk";
 import RecipePage from "../features/halaman-receipes";
@@ -8,6 +8,9 @@ import CartsPage from "../features/halaman-carts";
 export default function DashboardRoute() {
   return (
     <Route path="/" element={<DashboardLayout />}>
+      {/* Redirect dari "/" ke "/dashboard" */}
+      <Route index element={<Navigate to="dashboard" replace />} />
+
       <Route path="dashboard" element={<DashboardPages />} />
       <Route path="halamanProduk" element={<ProductList />} />
       <Route path="halamanrecipes" element={<RecipePage />} />
